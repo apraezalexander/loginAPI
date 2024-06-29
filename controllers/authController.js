b79df63e9d5df2bc6458ea1a2962454d38a918f3
alexander apraez <apraez.consultant@gmail.com>
2024-06-29 00:32:09 -0500

    loginAPI registro de usuario

---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcryptjs');
+const Joi = require('joi');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(50).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
+});
+
+exports.register = async (req, res) => {
+  const { error } = registerSchema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const { username, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ error: 'El usuario ya existe' });
+
+    user = new User({
+      username,
+      email,
+      password: await bcrypt.hash(password, 10)
+    });
+
+    await user.save();
+    res.status(201).json({ mensaje: 'Usuario registrado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+};
+
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
+});
+
+exports.login = async (req, res) => {
+  const { error } = loginSchema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: 'Credenciales inválidas' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: 'Credenciales inválidas' });
+
+    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+
+    res.status(200).json({ mensaje: 'Inicio de sesión exitoso', token });
+  } catch (error) {
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+};
---
+++ routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { register, login } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+// Ruta protegida de ejemplo
+router.get('/protected', authMiddleware, (req, res) => {
+    res.status(200).json({ mensaje: 'Accediste a una ruta protegida', user: req.user });
+  });
+
+module.exports = router;
